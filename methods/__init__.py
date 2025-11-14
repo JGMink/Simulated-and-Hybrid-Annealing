1af638ab39fe5ca607c40ac221acbf4a217a8fa7
JGMink <jonah@planetminkoff.com>
2025-11-14 13:33:14 -0500

    Deleted uneccassary files and innacurate method implementations.

--- methods/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Methods package for annealing scripts.
-
-This package contains refactored implementations of the original
-top-level scripts. Use the top-level wrappers to run the methods.
-"""
-
-__all__ = ["simulated_annealing", "hybrid_annealing"]
